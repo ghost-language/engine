2da46f62d9686cc924adf93d59bc54673cf85a13
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-06-14 15:53:30 -0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# engine
-TBA
+# Engine
+Engine is a simple framework to make 2D games in Ghost.
+
+## Status
+Currently messing around with SDL and experimenting on what the API should look like. Not in a usable state (Ghost itself isn't even complete for the scripting aspect to be built).
+
+## Notes
+
+### Built In Functions
+
+#### `load()`
+Called only once before the game loop begins. This is where you would want to pre-load any resources, initialize variables, and configure settings. While its possible to perform these actions elsewhere in your code, its generally a better idea to do so here as its not called at every frame.
+
+#### `update()`
+Called continuously where calculations and other deterministic factors should be performed. Will pass through the `delta time` allowing you to factor in the time since the last time this function was called. `delta time` is usually a very small number, like `0.025714`.
+
+#### `draw()`
+Much like `update`, this function is called continuously, allowing you to draw and update the screen.
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	title        = "Engine"
+	screenWidth  = 800
+	screenHeight = 600
+)
+
+var (
+	err      error
+	window   *sdl.Window
+	renderer *sdl.Renderer
+	event    sdl.Event
+	running  bool
+	ticker   *time.Ticker
+)
+
+func load() {
+	//
+}
+
+func update() {
+	//
+}
+
+func draw() {
+	renderer.SetDrawColor(247, 250, 252, 255)
+	renderer.Clear()
+
+	renderer.Present()
+}
+
+func main() {
+	if !initialize() {
+		os.Exit(1)
+	}
+
+	running = true
+	ticker = time.NewTicker(time.Second / 30)
+
+	load()
+
+	for running {
+		handleEvents()
+		update()
+		draw()
+		<-ticker.C
+	}
+
+	shutdown()
+}
+
+func initialize() bool {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+
+	if err != nil {
+		fmt.Println("initializing SDL:", err)
+		return false
+	}
+
+	w, err := sdl.CreateWindow(
+		title,
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		screenWidth,
+		screenHeight,
+		sdl.WINDOW_OPENGL)
+
+	if err != nil {
+		fmt.Println("initializing window:", err)
+		return false
+	}
+
+	window = w
+
+	r, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+
+	if err != nil {
+		fmt.Println("initializing renderer:", err)
+		return false
+	}
+
+	renderer = r
+
+	return true
+}
+
+func shutdown() {
+	renderer.Destroy()
+	window.Destroy()
+	sdl.Quit()
+}
+
+func handleEvents() {
+	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch event.(type) {
+		case *sdl.QuitEvent:
+			println("Quitting Engine...")
+			running = false
+			break
+		}
+	}
+}
